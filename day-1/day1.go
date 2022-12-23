b417b4c3922561c1bc9cc83d4f2465c6fb1291af
Yago Riveiro <yago.riveiro@gmail.com>
2022-12-23 13:31:19 +0000

    feat: day 1

---
+++ day-1/day1.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	var top int
+	var acc int
+	elf := 1
+
+	f, err := os.Open("input")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fs := bufio.NewScanner(f)
+
+	fs.Split(bufio.ScanLines)
+
+	for fs.Scan() {
+		if fs.Text() == "" {
+			fmt.Printf("Processing elf number '%d' with '%d' calories\n", elf, acc)
+
+			if acc > top {
+				top = acc
+			}
+
+			elf = elf + 1
+			acc = 0
+		}
+
+		c, err := strconv.Atoi(fs.Text())
+		if err == nil {
+			fmt.Printf("\tTotal caloires '%d', adding '%d' more\n", acc, c)
+			acc = acc + c
+		}
+	}
+
+	f.Close()
+	fmt.Printf("The elf with most calories has '%d'", top)
+}
